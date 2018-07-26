43784623520de0a67d6929aabaaf42357ce3e2e1
Hariharan <hariharanmdvn@gmail.com>
2018-07-26 12:11:58 +0530

    made changes in LongestPalindrome

--- LongestPalindrome.java
+++ LongestPalindrome.java
@@ -1,3 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mypackage;
+
 import java.util.Scanner;
 
 /**
@@ -26,7 +33,7 @@ public class LongestPalindrome {
                 }
             }
         }
-        System.out.println("After removing largest Pallindrome : "+string.replace(largestPalindrome,""));
+        System.out.println(string.replace(largestPalindrome,"").length());
     }
     private static boolean isPalindrome(String string)
     {
