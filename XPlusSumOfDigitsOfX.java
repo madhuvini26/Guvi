1f2a6c2e239bcb2325e656d1727fd4e04b2d5688
Hariharan <hariharanmdvn@gmail.com>
2018-08-06 11:53:13 +0530

    added new file XPlusSumOfDigitsOfX.java

---
+++ XPlusSumOfDigitsOfX.java
@@ -0,0 +1,43 @@
+package mypackage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Hariharan
+ */
+public class XPlusSumOfDigitsOfX {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Input : ");
+        long n=sc.nextInt();
+        List<Long> x=new ArrayList<>();
+        int count=0;
+        for(long i=n;i>=n/2;i--)
+        {
+            if(i+sumOfTheDigits(i) == n)
+            {
+                x.add(i);
+                count++;
+                break;
+            }
+        }
+        
+        System.out.println("Output :\n" + count);
+        for(int i=0;i<x.size();i++)
+            System.out.println(x.get(i));
+            
+    }
+    private static long sumOfTheDigits(long n)
+    {
+        long sum=0;
+        while(n!=0)
+        {
+            sum+=n%10;
+            n/=10;
+        }
+        return sum;
+    }
+}
