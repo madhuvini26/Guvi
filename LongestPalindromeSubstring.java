f4caf036449579c2f9802abaec3cb5b01ec817a9
Hariharan <hariharanmdvn@gmail.com>
2018-08-07 10:12:26 +0530

    added new file LongestPalindromeSubstring.java

---
+++ LongestPalindromeSubstring.java
@@ -0,0 +1,39 @@
+package mypackage;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Hariharan
+ */
+public class LongestPalindromeSubstring {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Input : ");
+        String string=sc.next();
+        String largestPalindrome=new String();
+        int maxLength=0;
+        
+        for(int i=0;i<string.length();i++)
+        {
+            for(int j=i+1;j<=string.length();j++)
+            {
+                String subString=string.substring(i,j);
+                if(isPalindrome(subString))
+                {
+                    if(subString.length() > maxLength)
+                    {
+                        largestPalindrome=subString;
+                        maxLength=subString.length();
+                    }
+                }
+            }
+        }
+        System.out.println("Output : \n" +largestPalindrome);
+    }
+    private static boolean isPalindrome(String string)
+    {
+        return string.equalsIgnoreCase((new StringBuilder(string).reverse()).toString());
+    }
+    
+}
