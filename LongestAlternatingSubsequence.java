43274115660ceb6d01b183088a2ef6d9e2fefbb9
Hariharan M <hariharanmdvn@gmail.com>
2018-08-12 19:31:55 +0530

    Update LongestAlternatingSubsequence.java

--- LongestAlternatingSubsequence.java
+++ LongestAlternatingSubsequence.java
@@ -1,3 +1,16 @@
+/*
+There's an array A consisting of N non-zero integers A1..N. A subarray of A is called alternating if any two adjacent elements 
+in it have different signs (i.e. one of them should be negative and the other should be positive). For each x from 1 to N, 
+compute the length of the longest alternating subarray that starts at x - that is, a subarray Ax..y for the maximum possible 
+y ≥ x. The length of such a subarray is y-x+1.
+Input Size : N<=100000 
+Example:
+INPUT
+4
+1 -5 1 -5
+OUTPUT
+4 3 2 1
+*/
 package mypackage;
 
 import java.util.Scanner;
