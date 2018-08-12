9f63a2eac19fe4ce429320103bebbd4ae9a05ae1
Hariharan M <hariharanmdvn@gmail.com>
2018-08-12 18:50:59 +0530

    Update KRoundingOfN.java

--- KRoundingOfN.java
+++ KRoundingOfN.java
@@ -1,4 +1,14 @@
-package mypackage;
+/*
+For a given positive integer N denote its K-rounding as the minimum positive integer x, such that x ends with k or more zeros 
+in base 10 and is divisible by n. For example, 4-rounding of 375 is 375*80 = 30000. 30000 is the minimum integer such that it 
+ends with 4 or more zeros and is divisible by 375. Write a program that will perform the k-rounding of N.
+Input Size : 1<=N<=1000000000, K<=8 
+Example:
+INPUT
+375 4
+OUTPUT
+30000
+*/package mypackage;
 
 import java.util.Scanner;
 
