6a86bdc193b755e433dd595fb9dacb122e3ec5fa
Hariharan M <hariharanmdvn@gmail.com>
2018-08-12 17:45:40 +0530

    Update Apartment.java

--- Apartment.java
+++ Apartment.java
@@ -1,3 +1,17 @@
+/*
+Maxwell wants to buy an apartment in a new house at Line Avenue of Metropolis. The house has n apartments that are numbered 
+from 1 to n and are arranged in a row. Two apartments are adjacent if their indices differ by 1. Some of the apartments can 
+already be inhabited, others are available for sale. Maxwell often visits his neighbors, so apartment is good for him if it 
+is available for sale and there is at least one already inhabited apartment adjacent to it. Maxwell knows that there are 
+exactly k already inhabited apartments, but he doesn't know their indices yet. Find out what could be the minimum possible 
+and the maximum possible number of apartments that are good for Maxwell.
+Input Size : N<=K<=1000000000
+Example:
+INPUT
+6 3
+OUTPUT
+1 3
+*/
 package mypackage;
 
 import java.util.Arrays;
