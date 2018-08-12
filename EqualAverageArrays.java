fa49187a4f101afc7606d096cedc545507639431
Hariharan M <hariharanmdvn@gmail.com>
2018-08-12 19:13:59 +0530

    Update EqualAverageArrays.java

--- EqualAverageArrays.java
+++ EqualAverageArrays.java
@@ -1,3 +1,13 @@
+/*
+Given a number N and an array of N numbers, find if it is possible to divide the array into 2 arrays with equal average.
+Input Size : 2<=N<=100000
+Example:
+INPUT
+5
+4 2 3 4 2
+OUTPUT
+yes
+*/
 package mypackage;
 
 import java.util.Scanner;
