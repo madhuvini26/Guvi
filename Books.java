9fdeccd502fc5c77b2f729d68b4e780966d7f62a
Hariharan M <hariharanmdvn@gmail.com>
2018-08-12 18:01:27 +0530

    Update Books.java

--- Books.java
+++ Books.java
@@ -1,3 +1,17 @@
+/*
+Recently Kohli bought a very interesting book. He knows that it will take t seconds to read the book. Kohli wants to finish 
+reading as fast as he can.But he has some work to do in each of n next days. The number of seconds that Kohli has to spend 
+working during i-th day is ai. If some free time remains, he can spend it on reading. Help Kohli to determine the minimum 
+number of day when He finishes reading. It is guaranteed that the answer doesn't exceed n. Remember that there are 86400 
+seconds in a day.
+Input Size : N<=100T<=1000000 
+example
+INPUT
+2 2
+86400 86398
+OUTPUT
+2
+*/
 package mypackage;
 
 import java.util.Scanner;
