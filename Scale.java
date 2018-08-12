9ce2874bb50131dc8476c5f46b29ba14b76f9dda
Hariharan M <hariharanmdvn@gmail.com>
2018-08-12 18:19:35 +0530

    Update Scale.java

--- Scale.java
+++ Scale.java
@@ -1,3 +1,21 @@
+/*
+59 -  Kedar has ordinary pan scales and several weights of an equal mass. Kedar has already put some weights on the scales, 
+while other weights are untouched. Kedar is now wondering whether it is possible to put all the remaining weights on the scales 
+so that the scales were in equilibrium. The scales is in equilibrium if the total sum of weights on the left pan is equal to 
+the total sum of weights on the right pan. The first line has a non-empty sequence of characters describing the scales. 
+In this sequence, an uppercase English letter indicates a weight, and the symbol '|' indicates the delimiter (the character 
+occurs in the sequence exactly once). All weights that are recorded in the sequence before the delimiter are initially on the 
+left pan of the scale. All weights that are recorded in the sequence after the delimiter are initially on the right pan of the 
+scale. The second line contains a non-empty sequence containing uppercase English letters. Each letter indicates a weight which 
+is not used yet. It is guaranteed that all the English letters in the input data are different. It is guaranteed that the input 
+does not contain any extra characters.
+Example:
+INPUT
+AC|T
+L
+OUTPUT
+AC|TL
+*/
 package mypackage;
 
 import java.util.Scanner;
