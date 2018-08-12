d09f0f4821d5f626d3f022c9f17deda580101452
Hariharan M <hariharanmdvn@gmail.com>
2018-08-12 18:54:40 +0530

    Update Soldiers.java

--- Soldiers.java
+++ Soldiers.java
@@ -1,3 +1,22 @@
+/*
+Two soldiers are playing a game. At the beginning first of them chooses a positive integer n and gives it to the second 
+soldier. Then the second one tries to make maximum possible number of rounds. Each round consists of choosing a positive integer 
+x > 1, such that n is divisible by x and replacing n with n / x. When n becomes equal to 1 and there is no more possible valid 
+moves the game is over and the score of the second soldier is equal to the number of rounds he performed. To make the game more 
+interesting, first soldier chooses n of form a! / b! for some positive integer a and b (a ≥ b). Here by k! we denote the 
+factorial of k that is defined as a product of all positive integers not large than k.
+What is the maximum possible score of the second soldier?
+The input consists of several test cases.
+Input Size : 1<=T,A,B<=100000 
+Example:
+INPUT
+2
+3 1
+6 3
+OUTPUT
+2
+5
+*/
 package mypackage;
 
 import java.util.Scanner;
