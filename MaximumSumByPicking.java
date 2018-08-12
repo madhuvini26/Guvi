1ea95ec9d2bf280c2f32f96c542295385322112e
Hariharan M <hariharanmdvn@gmail.com>
2018-08-12 19:14:51 +0530

    Update MaximumSumByPicking.java

--- MaximumSumByPicking.java
+++ MaximumSumByPicking.java
@@ -1,3 +1,15 @@
+/*
+Given a number N followed by an array of N numbers, find the maximum sum which can be obtained by picking the elements of the 
+array based on the following rule:
+'If you pick the current element u cannot pick its neighbors'.
+Input Size : 1<=N<=100000
+Example:
+INPUT
+5
+1 2 3 4 5
+OUTPUT
+9
+*/
 package mypackage;
 
 import java.util.Scanner;
