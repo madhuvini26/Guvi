575910e8803b1d04a431bd9c7962e3b30beaade0
Hariharan M <hariharanmdvn@gmail.com>
2018-08-12 19:38:47 +0530

    Update GCD.java

--- GCD.java
+++ GCD.java
@@ -1,3 +1,17 @@
+/*
+Given 2 numbers N,Q and an array of N integers, followed by Q queries each consisting of L,R. 
+Find the GCD of numbers in [L,R].
+Input Size : 1<=N<=100000
+Example:
+INPUT
+5 2
+1 2 3 4 5
+1 2
+4 5
+OUTPUT
+1
+1  
+*/
 package mypackage;
 
 import java.util.Scanner;
