6617219c2c88e3a190750a95f1aec74eee92b2d7
Hariharan M <hariharanmdvn@gmail.com>
2018-08-12 18:02:18 +0530

    Update Substring.java

--- Substring.java
+++ Substring.java
@@ -1,3 +1,13 @@
+/*
+57 - Design an algorithm to accept 2 strings A and B from the user, and check whether any part of string A 
+(comprising of minimum of 2 consecutive characters) is a sub-string of string B.
+Input Size : A<=1000, B<=1000
+Example:
+INPUT
+abcxyz hagsaabc
+OUTPUT
+yes
+*/
 package mypackage;
 
 import java.util.Scanner;
