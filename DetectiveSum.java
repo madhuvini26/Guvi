7fed0185768ce2ce41af49682dfd3837e3853ba0
Hariharan M <hariharanmdvn@gmail.com>
2018-08-12 19:39:37 +0530

    Update DetectiveSum.java

--- DetectiveSum.java
+++ DetectiveSum.java
@@ -1,3 +1,17 @@
+/*
+Once detective saikat was solving a murder case. While going to the crime scene he took the stairs and saw that a number 
+is written on every stair. He found it suspicious and decides to remember all the numbers that he has seen till now. 
+While remembering the numbers he found that he can find some pattern in those numbers. So he decides that for each number 
+on the stairs he will note down the sum of all the numbers previously seen on the stairs which are smaller than the present 
+number. Calculate the sum of all the numbers written on his notes diary.
+Input Size : 1<=N<=100000
+Example:
+INPUT
+3
+1 2 3
+OUTPUT
+4(0+1+1+2) 
+*/
 package mypackage;
 
 import java.util.Scanner;
