1e2cb64dcdd28eb6eb48275d66d2e79a83d9fb43
Hariharan M <hariharanmdvn@gmail.com>
2018-08-13 09:25:46 +0530

    Update AlternateSorting.java

--- AlternateSorting.java
+++ AlternateSorting.java
@@ -1,3 +1,13 @@
+/*
+Alternate sorting: Given a number N and followed by N numbers, sort the array in such a way that the first number is first maximum and second number is 1st minimum 3rd number is 2nd maximumand so on..
+Input Size : N <= 100000
+Sample Testcase :
+INPUT
+8
+7 623 19 10 11 9 3 15
+OUTPUT
+623 3 19 7 15 9 11 10
+*/
 package mypackage;
 
 import java.util.Arrays;
